d50dbb9d4d0ef14618a3404d5fe207484e8d27a3
Misha1542 <154328571+Misha1542@users.noreply.github.com>
2026-08-16 16:25:23 +0300

    Update launcher.py

--- ZedLauncher/launcher.py
+++ ZedLauncher/launcher.py
@@ -5,8 +5,8 @@ class OOP:
     self.text = text
     self.ekran = ekran
   def create_label(self):
-         name = tk.Label(self.ekran, text=self.text)
-         name.pack()
+    name = tk.Label(self.ekran, text=self.text)
+    name.pack()
     
 jopa = OOP(text="жопа", ekran=root)
 jopa.create_label()
