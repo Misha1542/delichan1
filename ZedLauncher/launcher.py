bd509dcba3ea45b6bb6f7cea5a44e10a70fea597
Misha1542 <154328571+Misha1542@users.noreply.github.com>
2026-08-16 16:14:09 +0300

    Update launcher.py

--- ZedLauncher/launcher.py
+++ ZedLauncher/launcher.py
@@ -1,11 +1,14 @@
 import tkinter as tk
 root = tk.Tk() 
 class OOP:
-  def __init__(self, name, text):
+  def __init__(self, name, text, ekran):
     self.name = name
     self.text = text
+    self.ekran = ekran
   def create_label(self):
-    self.name = tk.Label(text=self.text)
+    self.name = tk.Label(self.ekran, text=self.text)
     self.name.pack()
-jopa = OOP('label', 'жопа')
+    
+jopa = OOP(name= "label", text="жопа", ekran=root)
+jopa.create_label()
 root.mainloop()
